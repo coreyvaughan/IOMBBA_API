52ae492007c41cc57f89490ffe18720196b9da25
coreyvaughan <coreytvaughan@gmail.com>
2020-05-04 11:49:43 +0100

    initial commit

---
+++ IOMBBA_API/Controllers/MemberController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Objects.Interfaces;
+using Objects.Models;
+using Objects.Objects;
+
+namespace IOMBBA_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+
+    public class MemberController : ControllerBase
+    {
+        // Declare the interface.
+        private readonly IMember memberService;
+
+        public MemberController(IMember context)
+        {
+            memberService = context;
+        }
+
+        /// <summary>
+        /// Get all member records held on the database.
+        /// </summary>
+        /// <returns>A list of member models</returns>
+        [HttpGet]
+        [Route("GetAllMembers")]
+        public List<MemberModel> GetAllMembers()
+        {
+            return memberService.GetAllMembers();
+        }
+
+        /// <summary>
+        /// Gets the details of all members on the database
+        /// </summary>
+        /// <returns>A list of member details objects</returns>
+        [HttpGet]
+        [Route("GetAllMemberDetails")]
+        public List<MemberDetailsObject> GetAllMemberDetails()
+        {
+            return memberService.GetAllMemberDetails();
+        }
+
+        /// <summary>
+        /// Gets the details of a member for a given id.
+        /// </summary>
+        /// <param name="id">The id of the member</param>
+        /// <returns>A MemberDetailsObject</returns>
+        [HttpGet]
+        [Route("GetMemberDetails/{id}")]
+        public MemberDetailsObject GetMemberDetails(int id)
+        {
+            return memberService.GetMemberDetails(id);
+        }
+
+        /// <summary>
+        /// Gets a member for a given id.
+        /// </summary>
+        /// <param name="id">The id of the member</param>
+        /// <returns>An action result</returns>
+        [HttpGet("{id}")]
+        [Route("GetMember/{id}")]
+        public async Task<MemberModel> GetMember([FromRoute] int id)
+        {
+            return await memberService.GetMember(id);
+        }
+
+        /// <summary>
+        /// Edits an existing member record on the database.
+        /// </summary>
+        /// <param name="id">The id of the member</param>
+        /// <param name="form">The member details, passed in from the form</param>
+        /// <returns>The new values of the edited record</returns>
+        [HttpPut("{id}")]
+        [Route("EditMember/{id}")]
+        public async Task<MemberModel> EditMember([FromRoute] int id, [FromBody] MemberObject form)
+        {
+            return await memberService.EditMember(id, form);
+        }
+
+        /// <summary>
+        /// Adds a new member record to the database.
+        /// </summary>
+        /// <param name="form">The details of the new member to be added</param>
+        /// <returns>An action result</returns>
+        [HttpPost]
+        [Route("AddMember")]
+        public async Task<MemberModel> AddMember([FromBody] MemberObject form)
+        {
+            return await memberService.AddMember(form);
+        }
+
+        /// <summary>
+        /// Deletes an existing member from the database.
+        /// </summary>
+        /// <param name="id">The id of the member to be deleted</param>
+        /// <returns>An action result</returns>
+        [HttpDelete("{id}")]
+        [Route("DeleteMember/{id}")]
+        public async Task DeleteMember([FromRoute] int id)
+        {
+            await memberService.DeleteMember(id);
+        }
+    }
+}
---
+++ IOMBBA_API/Startup.cs
@@ -0,0 +1,88 @@
+﻿using EntityFramework;
+using IOMBBA_System.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Objects.Interfaces;
+using Objects.Models;
+
+namespace IOMBBA_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                });
+            services.AddDbContextPool<DbContextClass>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            // Dependency injection for interfaces.
+            services.AddScoped<ICompetition, CompetitionRepository>();
+            services.AddScoped<IFixture, FixtureRepository>();
+            services.AddScoped<IGameplayStat, GameplayStatRepository>();
+            services.AddScoped<IMember, MemberRepository>();
+            services.AddScoped<ITeam, TeamRepository>();
+
+
+
+
+
+
+
+
+            // CORS policy - allow all to access.
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                  builder =>
+                  {
+                      builder
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+                  });
+            });
+            //services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("AllowAny");
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
